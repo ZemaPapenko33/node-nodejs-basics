fa4b42045d01a0d5ce52d311b878e936ae2f5daf
Zahar Papenko <zakharpapenko@gmail.com>
2025-04-27 23:44:46 +0300

    feat: add solve for hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import crypto from "crypto";
+import { pipeline } from "stream";
+import { promisify } from "util";
+
+// Преобразуем pipeline в промис для асинхронного использования
+const pipe = promisify(pipeline);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const hash = crypto.createHash("sha256");
+    const readStream = fs.createReadStream(
+      "./files/fileToCalculateHashFor.txt"
+    );
+
+    await pipe(readStream, hash);
+
+    console.log(hash.digest("hex"));
+  } catch (error) {
+    console.error("Error during hash calculation:", error);
+  }
 };
 
 await calculateHash();
