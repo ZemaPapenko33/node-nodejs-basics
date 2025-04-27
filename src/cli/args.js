f0b4c48490cfec5330a1993ed506fafd7c094675
Zahar Papenko <zakharpapenko@gmail.com>
2025-04-27 15:47:06 +0300

    feat: solve cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const keyRegex = /^--prop.*Name$/;
+
+  for (let i = 2; i < process.argv.length; i += 2) {
+    const key = process.argv[i];
+    const value = process.argv[i + 1];
+    if (!keyRegex.test(key)) {
+      throw new Error(`Invalid key format: ${key}`);
+    }
+    if (!value || value.trim() === "") {
+      throw new Error(`Value for ${key} is empty`);
+    }
+
+    console.log(`${key}=${value}`);
+  }
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  for (const startString in process.env) {
+    if (startString.startsWith("RSS_")) {
+      console.log(`${startString}=${process.env[startString]}`);
+    }
+  }
 };
 
 parseEnv();
