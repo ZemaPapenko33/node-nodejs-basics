19690f33d3bef8d3bbd32e819e7b057854cbc937
Zahar Papenko <zakharpapenko@gmail.com>
2025-04-28 19:08:18 +0300

    feat: add solve for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,28 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  try {
+    const cpuData = os.cpus().length;
+    const promises = [];
+    for (let i = 0; i < cpuData; i++) {
+      promises.push(
+        new Promise((resolve) => {
+          const worker = new Worker("./worker.js", { workerData: 10 + i });
+
+          worker.on("message", (data) => {
+            resolve({ status: "resolved", data });
+          });
+
+          worker.on("error", () => {
+            resolve({ status: "error", data: null });
+          });
+        })
+      );
+    }
+    const settledResult = await Promise.all(promises);
+    console.log(settledResult);
+  } catch {}
 };
 
 await performCalculations();
