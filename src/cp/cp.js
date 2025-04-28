589aff65dc8563a605c80e3c5144543fd6eb7c3f
Zahar Papenko <zakharpapenko@gmail.com>
2025-04-28 19:30:51 +0300

    feat:solve for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,18 @@
+import { spawn } from "child_process";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childStream = spawn("node", ["./files/script.js", ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(childStream.stdin);
+
+  childStream.stdout.pipe(process.stdout);
+
+  childStream.on("error", (err) => {
+    console.error("Error spawning child process:", err);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3, 4, 5]);
