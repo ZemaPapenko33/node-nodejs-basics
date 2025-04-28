ace7808575dafb264c73475457b1f036f591ac3e
Zahar Papenko <zakharpapenko@gmail.com>
2025-04-28 13:16:35 +0300

    feat: solve zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream("./files/fileToCompress.txt");
+    const compressStream = zlib.createGzip();
+    const writeStream = fs.createWriteStream("./files/archive.gz");
+
+    readStream.on("error", () => {
+      throw new Error("Read stream error");
+    });
+    compressStream.on("error", () => {
+      throw new Error("Compress stream error");
+    });
+    writeStream.on("error", () => {
+      throw new Error("Write stream error");
+    });
+
+    readStream.pipe(compressStream).pipe(writeStream);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  try {
+    const readStream = fs.createReadStream("./files/archive.gz");
+    const writeStream = fs.createWriteStream("./files/fileToCompress.txt");
+    const decompressStream = zlib.createGunzip();
+
+    readStream.on("error", () => {
+      throw new Error("Read stream error");
+    });
+    decompressStream.on("error", () => {
+      throw new Error("Compress stream error");
+    });
+    writeStream.on("error", () => {
+      throw new Error("Write stream error");
+    });
+
+    readStream.pipe(decompressStream).pipe(writeStream);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await decompress();
